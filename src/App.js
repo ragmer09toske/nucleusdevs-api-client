c2f7ba3b23e0ecdae302ac4fc8bc1af70aba8cf8
ragmer09toske <73499664+ragmer09toske@users.noreply.github.com>
2023-08-12 10:40:49 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Login from './Login';
 import Protected from './Protected';
-
+import './App.css';
 function App() {
   const [view, setView] = useState(<Login />)
 
   useEffect(()=>{
     const token = localStorage.getItem('token');
-    token ? setView(<Protected/>) : localStorage.removeItem('token')
+    token && setView(<Protected/>)
   },[])
 
   return <div>{view}</div>;
